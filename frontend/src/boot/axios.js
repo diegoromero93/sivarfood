624869d6ea7dbc6b51daaa30bc7092a66ea50849
Diego Romero <diegor.romero@yahoo.com>
2020-08-28 21:39:50 -0600

    añadiendo nueva rama para manejar versiones

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "http://api.sivarfood.siliconsivar.com";
 axios.defaults.withCredentials = true;
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false;
